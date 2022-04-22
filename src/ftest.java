96ff425b048fa1d0dd876debe44dd85a590a8ad4
wjc <15584932588@163.com>
2022-04-22 23:59:28 +0800

    test

--- src/ftest.java
+++ src/ftest.java
@@ -1,5 +1,11 @@
 public class ftest {
     public static void main(String[] args) {
         System.out.println(111111);
+
+        System.out.println(22222222);
+
+
+        System.out.println(3333333);
+
     }
 }
